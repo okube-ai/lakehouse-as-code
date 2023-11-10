215a37d3d66dbc97c011056eb95710a01e4b4d7d
osoucy <osoucy.transactions@gmail.com>
2023-11-10 11:25:20 -0500

    Update

--- gcp-infra/__main__.py
+++ gcp-infra/__main__.py
@@ -21,8 +21,9 @@ class Service:
         self.databricks_workspace_provider = None
 
         # Resources
-        self.app = None
-        self.app_secret = None
+        # self.app = None
+        # self.app_secret = None
+        self.service_account = None
         self.sp = None
         self.me = None
         self.rg = None
@@ -32,22 +33,53 @@ class Service:
         self.workspace = None
 
     def run(self):
-        # Service principals
-        # self.set_service_principal()
+
+        # https://docs.gcp.databricks.com/dev-tools/terraform/gcp-workspace.html
+
+        # Service account
+        self.set_service_account()
         #
         # Key Vault
         # self.set_keyvault()
-
-        # https://docs.gcp.databricks.com/dev-tools/terraform/gcp-workspace.html
 
         # Storage Account
         self.set_storage()
 
         # Databricks workspace
-        self.set_workspace()
+        # self.set_workspace()
         #
         # # Databricks connector
         # self.set_databricks_connector()
+
+    # ----------------------------------------------------------------------- #
+    # Service Account                                                         #
+    # ----------------------------------------------------------------------- #
+
+    def set_service_account(self):
+        self.service_account = gcp.serviceaccount.Account(
+            "neptune",
+            account_id="neptune",
+            display_name="neptune",
+        )
+        # pulumi.export("tenant-id", self.tenant_id)
+        pulumi.export("neptune-email", self.service_account.email)
+        # pulumi.export("neptune-client-id", self.app.application_id)
+        #
+        #  gcp.iam.Policy(
+        #     "aad-sp-neptune",
+        #     application_id=self.app.application_id,
+        # )
+        #
+        # self.app_secret = azuread.ApplicationPassword(
+        #     "aad-app-secret-neptune",
+        #     application_object_id=self.app.object_id,
+        #     end_date_relative=f"{24*30*6}h",
+        # )
+        # pulumi.export("neptune-client-secret", self.app_secret.value)
+
+    # ----------------------------------------------------------------------- #
+    # Storage                                                                 #
+    # ----------------------------------------------------------------------- #
 
     def set_storage(self):
         k = f"{self.org}-bucket-{self.service}-landing"
@@ -109,26 +141,6 @@ class Service:
         # )
         # pulumi.export(f"dbks-ws-host", self.workspace.workspace_url)
         # pulumi.export(f"dbks-ws-name", self.workspace.workspace_name)
-        #
-        # self.databricks_workspace_provider = databricks.Provider(
-        #     "workspace",
-        #     host=self.workspace.workspace_url,
-        #     username=self.databricks_config.get("username"),
-        #     password=self.databricks_config.get("password"),
-        # )
-        #
-        # # Token
-        # name = self.rnp.get_name(ResourceTypes.DBKS_WORKSPACE_PAT, "lakehouse")
-        # self.workspace_pat = databricks.Token(
-        #     name,
-        #     comment="pulumi-ci-cd",
-        #     lifetime_seconds=3600*24*364,
-        #     opts=pulumi.ResourceOptions(
-        #         provider=self.databricks_workspace_provider,
-        #         depends_on=[self.workspace],
-        #     ),
-        # )
-        # pulumi.export(f"dbks-ws-token", self.workspace_pat.token_value)
 
 
 # --------------------------------------------------------------------------- #
