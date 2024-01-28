e0600578d04294962e3cf792de253b4d9b7cb0dd
Olivier Soucy <62808289+osoucy@users.noreply.github.com>
2024-01-28 00:34:27 -0500

    Laktory update (#22)

--- workspace/notebooks/pipelines/dlt_brz_template.py
+++ workspace/notebooks/pipelines/dlt_brz_template.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC #%pip install git+https://github.com/okube-ai/laktory.git@docs_update
-# MAGIC %pip install 'laktory==0.1.0'
+# MAGIC %pip install 'laktory==0.1.1'
 
 # COMMAND ----------
 import pyspark.sql.functions as F
